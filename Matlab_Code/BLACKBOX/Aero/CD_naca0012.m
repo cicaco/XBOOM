87ed05ad8c7a0753a476de46fbe517ac4bbffa98
RaffaeleTirotta <raffaele.tirotta@mail.polimi.it>
2022-06-10 15:35:34 +0200

    NACA 0012 updated aero coeff

---
+++ Matlab_Code/BLACKBOX/Aero/CD_naca0012.m
@@ -0,0 +1,29 @@
+function [CD]=CD_naca0012(AoA)
+%CL compute the CL for an angle of attack range -180/180
+%CL=CL(AoA) computes the lift coefficent for a given AoA
+% INPUT: AoA (Angle of Attack in rad);
+% OUTPUT: CL (Adimensional Lift coefficient);
+% Computed from XFOIL NACA 0012 coefficients
+
+%load computed quantity from XFoil
+data = load('reverse_0012_coeff');
+load norm_polar
+
+alpha_eval=AoA*180/pi;
+if alpha_eval<-180 || alpha_eval>180
+    fprintf('error')
+end
+
+%sorting alpha
+alpha   = data(:,1);
+alpha_m = -flip(data(:,1));
+alpha_t = [ alpha-180; linspace(-160,-40,6)'; ...
+    pol.alpha; linspace(40, 160, 6)'; alpha_m+180] ;
+
+%sorting CD
+CD_n      = data(:,3);
+CD_m      = flip(data(:,3));
+CD_t    = [  CD_n; 1.2*ones(6,1); ...
+     pol.CD; 1.2*ones(6,1); CD_m];
+% interpolation
+CD = interp1(alpha_t, CD_t, alpha_eval);
