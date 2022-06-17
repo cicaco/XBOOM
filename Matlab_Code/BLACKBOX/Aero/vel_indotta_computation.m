c0418c932f168f2389850d09a67e43970544955f
SimoneCruciani <simocruciani@gmail.com>
2022-06-17 10:02:11 +0200

    Ottimizzazione calcolo vel_indotta

--- Matlab_Code/BLACKBOX/Aero/vel_indotta_computation.m
+++ Matlab_Code/BLACKBOX/Aero/vel_indotta_computation.m
@@ -1,6 +1,6 @@
 function [vel_ind]=vel_indotta_computation(u,omega,v_ind_old,BoomInfo)
 
-psi_ciclo=linspace(0,2*pi,40);
+psi_ciclo=linspace(0,2*pi,30);
 psi_vect=midspan(psi_ciclo);
 
 %% import geometria
@@ -48,7 +48,7 @@ Tj2=[sin(lambda)*cos(pitch)+cos(lambda)*sin(coning)*sin(pitch), -cos(lambda)*cos
     sin(lambda)*sin(pitch)-cos(lambda)*sin(coning)*cos(pitch), -cos(lambda)*sin(pitch)-sin(lambda)*sin(coning)*cos(pitch), cos(coning)*cos(pitch)];
 
 F_ciclopsi=[];
-M_ciclopsi=[];
+% M_ciclopsi=[];
 for j=1:length(psi_vect)
     psi=psi_vect(j);
 T_ciclopsi=[cos(psi), sin(psi), 0
@@ -90,37 +90,37 @@ end
 %BET
 for i=1:length(eta1)
     fa1=(span1(i+1)-span1(i))*0.5*1.225*c*norm(w1([1 3],i))^2*[-CL(AoA1(i))*sin(AoA1(i))+CD(AoA1(i))*cos(AoA1(i)); 0; CL(AoA1(i))*cos(AoA1(i))+CD(AoA1(i))*sin(AoA1(i))];
-    ma1=(span1(i+1)-span1(i))*0.5*1.225*c*norm(w1([1 3],i))^2*[(CL(AoA1(i))*cos(AoA1(i))+CD(AoA1(i))*sin(AoA1(i)))*eta1(i); c*CM(AoA1(i)); (CL(AoA1(i))*sin(AoA1(i))-CD(AoA1(i))*cos(AoA1(i)))*eta1(i)];
+%     ma1=(span1(i+1)-span1(i))*0.5*1.225*c*norm(w1([1 3],i))^2*[(CL(AoA1(i))*cos(AoA1(i))+CD(AoA1(i))*sin(AoA1(i)))*eta1(i); c*CM(AoA1(i)); (CL(AoA1(i))*sin(AoA1(i))-CD(AoA1(i))*cos(AoA1(i)))*eta1(i)];
     fa2=(span2(i+1)-span2(i))*0.5*1.225*c*norm(w2([1 3],i))^2*[-CL(AoA2(i))*sin(AoA2(i))+CD(AoA2(i))*cos(AoA2(i)); 0; CL(AoA2(i))*cos(AoA2(i))+CD(AoA2(i))*sin(AoA2(i))];
-    ma2=(span2(i+1)-span2(i))*0.5*1.225*c*norm(w2([1 3],i))^2*[(CL(AoA2(i))*cos(AoA2(i))+CD(AoA2(i))*sin(AoA2(i)))*eta2(i); c*CM(AoA2(i)); (CL(AoA2(i))*sin(AoA2(i))-CD(AoA2(i))*cos(AoA2(i)))*eta2(i)];
+%     ma2=(span2(i+1)-span2(i))*0.5*1.225*c*norm(w2([1 3],i))^2*[(CL(AoA2(i))*cos(AoA2(i))+CD(AoA2(i))*sin(AoA2(i)))*eta2(i); c*CM(AoA2(i)); (CL(AoA2(i))*sin(AoA2(i))-CD(AoA2(i))*cos(AoA2(i)))*eta2(i)];
     %forze su sdr body
     f1=(Tj1')*fa1;
-    m1=(Tj1')*ma1+cross(xac1_ciclopsi,(Tj1')*fa1);
+%     m1=(Tj1')*ma1+cross(xac1_ciclopsi,(Tj1')*fa1);
     f2=Tj2'*fa2;
-    m2=Tj2'*ma2+cross(xac2_ciclopsi,(Tj2')*fa2);
+%     m2=Tj2'*ma2+cross(xac2_ciclopsi,(Tj2')*fa2);
     
     %aggiornamento variabili
 %     FA1=[FA1, fa1 ];
 %     MA1=[MA1, ma1];
     F1=[F1,f1];
-    M1=[M1,m1];
+%     M1=[M1,m1];
 %     FA2=[FA2, fa2];
 %     MA2=[MA2, ma2];
     F2=[F2, f2];
-    M2=[M2, m2 ];
+%     M2=[M2, m2 ];
 end
 
 F1=sum(F1,2);
-M1=sum(M1,2);
+% M1=sum(M1,2);
 
 F2=sum(F2,2);
-M2=sum(M2,2);
+% M2=sum(M2,2);
 
 F=F1+F2;
-M=M1+M2;
+% M=M1+M2;
 
 F_ciclopsi=[F_ciclopsi F];
-M_ciclopsi=[M_ciclopsi M];
+% M_ciclopsi=[M_ciclopsi M];
 end
 
 %calcolo v_indotta
