6e4a8e27a4054cb922db721a37198d076a6d5ec5
RaffaeleTirotta <raffaele.tirotta@mail.polimi.it>
2022-06-24 17:02:13 +0200

    NaN fastcatch profile loaded

---
+++ Matlab_Code/Main_GA_fastcatch.m
@@ -0,0 +1,130 @@
+%% Script per creare la Geometria 3D
+clear all 
+close all
+addpath(genpath('BLACKBOX'));
+%% Input Data
+Chord=0.0488;
+p_c=20; % numero di profili di "Transizione" nella parte centrale
+l=0.3; % lunghezza della pala avente un profilo 2D definito, NON corrisponde alla lunghezza del boomerang
+delta=30*pi/180; %Angolo di freccia
+beta=0.5*pi/180; %Angolo di Diedro
+pitch=4*pi/180; %Pitch angle
+num=20; %Numero di profili totale su ciascuna metà;
+PARA=2.; %Parametro che permette di modificare la curvatura centrale (più si avvicna ad 1 pù dietro forma una V
+% Profile 2D Shape
+%% Profilo 2D flip e analisi
+Profile2D=importdata('fastcatch.dat');
+% Xp=-[0; fliplr(Profile2D(1:65,1)')' ; fliplr(Profile2D(66:end,1)')'].*Chord;
+% Zp=[0 ;fliplr(Profile2D(1:65,2)')' ; fliplr(Profile2D(66:end,2)')'].*Chord;
+% Xp_flip=-(Chord/2.*ones(size(Xp))+Xp)+Chord/2.*ones(size(Xp))-Chord;
+% Zp_flip=(Zp);
+% [n,~]=size(Xp);
+% %Clock-wise direction regeneration
+% Xp_flip=[fliplr(Xp_flip(1:n/2)')';fliplr(Xp_flip(n/2+1:end)')'];
+% Zp_flip=[fliplr(Zp_flip(1:n/2)')';fliplr(Zp_flip(n/2+1:end)')'];
+Xp      = Profile2D.data(:,1).*Chord;
+Zp      = Profile2D.data(:,2).*Chord;
+Xp_flip = -Xp +Chord;
+Zp_flip = Zp;
+figure()
+plot(Xp,Zp,'r');
+hold on
+plot(Xp_flip,Zp_flip,'b');
+axis equal
+set(gca,'Xdir','reverse')
+%% Creazione dell Info Box
+load fastcatch_360.mat
+CL_t      = coeff360.CL;
+CD_t      = coeff360.CD*0;
+CM_t      = coeff360.CM;
+alpha_cl  = coeff360.alpha*pi/180;
+alpha_cd  = coeff360.alpha*pi/180;
+alpha_cm  = coeff360.alpha*pi/180;
+figure()
+plot(alpha_cl,CL_t,'--r');
+hold on
+plot(alpha_cd,CD_t,'.-b');
+plot(alpha_cm,CM_t,'k');
+grid on
+legend('CL','CD','CM');
+BoomInfo.Aero.alpha_cl=alpha_cl;
+BoomInfo.Aero.alpha_cd=alpha_cd;
+BoomInfo.Aero.alpha_cm=alpha_cm;
+BoomInfo.Aero.Cl=CL_t;
+BoomInfo.Aero.Cd=CD_t;
+BoomInfo.Aero.Cm=CM_t;
+%% Creazione dell Info Box
+BoomInfo.Pianta.l=l;
+BoomInfo.Pianta.freccia=delta;
+BoomInfo.Pianta.diedro=beta;
+BoomInfo.Pianta.pitch=pitch;
+BoomInfo.Geom3D.p_c=p_c;
+BoomInfo.Geom3D.num=num;
+BoomInfo.Geom3D.PARA=PARA;
+BoomInfo.Profile.Chord=Chord;
+BoomInfo.Profile.Xp_dx=Xp;
+BoomInfo.Profile.Xp_sx=Xp_flip;
+BoomInfo.Profile.Zp_dx=Zp;
+BoomInfo.Profile.Zp_sx=Zp_flip;
+
+%%
+n=num+p_c;
+R=1000;
+Dens_i=[R.*ones(1,n-p_c-1) R*ones(1,2*p_c-1) R.*ones(1,n-p_c-1)];
+[BoomInfo] = Boom3DShape(BoomInfo,'Info','Density_variation',Dens_i);
+BoomInfo.Mecc.I_rho
+BoomInfo.Mecc.m
+%% Initial condition
+fprintf('Starting Genetic Alghoritm...\n');
+% x=[r0 theta D phi Vs];
+lb=[8 0 0 0 8]*10; %[Hz gradi m/s]
+ub=[12 10  90  90 15]*10;
+fitnessfcn=@(x) GA_para1(x,BoomInfo);
+options = optimoptions('ga', 'MaxStallGenerations', 10, 'MaxGenerations', 10, 'NonlinearConstraintAlgorithm', 'penalty',...
+    'PopulationSize', 50, 'PlotFcn', {'gaplotbestindiv', 'gaplotbestf'},...
+    'Display', 'iter', 'UseParallel', true, 'UseVectorized', false,'FitnessLimit',4 );
+X_ini = ga(fitnessfcn,5,[],[],[],[],lb,ub,[],1:5,options);
+
+%%
+
+%% Initial condition
+[PAR] = GA_para1(X_ini,BoomInfo,'Ciao');
+X_ini_right=[X_ini(1)/10 X_ini(2)/10 X_ini(3)/10 X_ini(4)/10 X_ini(5)/10]
+
+%%
+r0=X_ini(1)*2*pi/10;
+theta=X_ini(2)*pi/180/10;
+D=X_ini(3)*pi/180/10;
+phi=X_ini(4)*pi/180/10;
+Vs=X_ini(5)/10;
+
+
+theta0=0*pi/180;
+phi0=0*pi/180;
+psi0=0*pi/180;
+Tl_0=[cos(theta0)*cos(psi0), cos(theta0)*sin(psi0), -sin(theta0)
+    -cos(phi0)*sin(psi0)+sin(phi0)*sin(theta0)*cos(psi0), cos(phi0)*cos(psi0)+sin(phi0)*sin(theta0)*sin(psi0), sin(phi0)*cos(theta0)
+    sin(phi0)*sin(psi0)+cos(phi0)*sin(theta0)*cos(psi0), -sin(phi0)*cos(psi0)+cos(phi0)*sin(theta0)*sin(psi0), cos(phi0)*cos(theta0)];
+z0= 1.8; % initial altitude
+[quat,ustart] = HandInitial(r0,theta,D,phi,Vs,Tl_0,BoomInfo);
+
+tfin=40;
+
+
+%[V_dx_b,V_sx_b]=InitialConditionPlot(Tl_0,T0,ustart,[0;0;r0],BoomInfo);
+[V_dx_b,V_sx_b]=InitialConditionPlot(Tl_0,quatToAtt(quat),ustart',[0;0;r0],BoomInfo);
+
+
+options = odeset('Events', @EventsQUAT,'RelTol',1e-4,'AbsTol',1e-6);
+Y0=[quat 0 0 r0  ustart(1) ustart(2) ustart(3) 0 0 z0 ]';
+%%
+%%
+tic
+[TOUT,YOUT_quat] = ode45(@(t,y)EquationOfMotionsQuaternion(t,y,BoomInfo,Tl_0),[0 tfin],Y0,options); %
+toc
+[YOUT] = Eul_Quat(YOUT_quat,TOUT);
+Energy(TOUT,YOUT,BoomInfo)
+PlotTipDxSx(TOUT,YOUT,BoomInfo,Tl_0)
+%PlotAeroForce(YOUT,TOUT,BoomInfo)
+ChiAvan(BoomInfo,YOUT,TOUT)
+FinalReport(YOUT,TOUT)
