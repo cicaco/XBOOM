b652282aa9cbc737c45f9db87bfb6c039424f18c
RaffaeleTirotta <raffaele.tirotta@mail.polimi.it>
2022-06-25 20:52:48 +0200

    Main script coupling with f_polar_360 in Main_FastCatch_360_coupled.m(it works -> boomerang comes back), bug fixing in 360 coefficients computation

---
+++ Matlab_Code/BLACKBOX/Airfoil_360/f_polar_360.m
@@ -0,0 +1,125 @@
+function coeff360 = f_polar_360(lon, airfoil_name, Re, CD_90) 
+% coeff360 = F_POLAR_360(lon, airfoil_name, Re)
+% WARNING -> PUT XFOIL IN THE SAME FOLDER OF THIS SCRIPT AND XFOIL.M
+% INPUT
+% lon            : string can be set to 'load' or 'NACA'
+% airfoil_name   : is the airfoil filename, if lon == NACA, just enter a
+%                  string with the serie, ex. '0012'
+% Re             :  Reynolds number for which you want to perform the 
+%                   computation
+% CD_90          : value of drag expected at 90 deg 
+% OUTPUT
+% coeff360       : struct filled with 360 degree polar info
+% coeff360.CL    : CL vector
+% coeff360.CD    : CD vector
+% coeff360.CM    : CM vector
+% coeff360.alpha : AoA vector [deg]
+% The function first recall xFoil, then extrapolate low AoA info in deep
+% stall region, using the flat plate analoguos
+% FLAT PLATE ANALOGOUS FOR CL, CD, CM -> for deep stall airfoil
+% CAMBER EFFECTS ARE INCLUDED for CL and CM
+% ALGORITHM IS BASE ON BJORN MONTGOMERIE MOST FAMOUS PAPER(THE ONE 
+% IMPLEMENTED IN QBLADE)
+% CAREFULL CD90 (CD AT 90 DEGREE MUST BE SPECIFIED) -> TOTALLY ARBITRARY
+% EXTENSION IN MAINLY BASED ON EXPERIMENTAL RESULTS AND FLT PLATE ANALOGOUS
+
+a       = linspace(-20,20, 41);
+Mach    = 0;
+
+if lon == "NACA"
+    airfoil = strcat("NACA", airfoil_name);
+elseif lon == "load"
+    airfoil = airfoil_name;
+else
+    error("Input lon not recognized, it can only be 'load' or 'NACA'")
+    %airfoil_name = 'fastcatch'
+    %airfoil = 'fastcatch.dat'
+end
+[pol,foil] = xfoil(airfoil,a,Re,Mach,'panels n 330', 'oper iter 1000')
+
+% find AoA stall
+alpha_ck = 4 % I really hope stall occur after 4 degree
+delta = 10;
+while delta > 0;
+    cl_check = interp1(pol.alpha, pol.CL, [alpha_ck, alpha_ck+1]);
+    %when cl changes sign -> alpha_stall
+    delta = cl_check(2) - cl_check(1);
+    alpha_ck = alpha_ck +1;
+end
+alpha_stall = alpha_ck -1 ;
+
+% compute negative stall
+alpha_ck = -4 % I really hope stall occur after -4 degree
+delta = -10;
+while delta < 0;
+    cl_check = interp1(pol.alpha, pol.CL, [alpha_ck, alpha_ck-1]);
+    %when cl changes sign -> alpha_stall
+    delta = cl_check(2) - cl_check(1);
+    alpha_ck = alpha_ck -1;
+end
+alpha_mstall = alpha_ck +1 ;
+
+% ok let's take 5 angles more after stall
+a       = linspace(alpha_mstall-5, alpha_stall+5, (alpha_stall-alpha_mstall)+11);
+[pol,foil] = xfoil(airfoil,a,Re,Mach,'panels n 330', 'oper iter 1000')
+%% alghorithm positive side
+%retrieve clalpha
+a_vec = (min(pol.alpha)+7):(max(pol.alpha)-7);
+% prendo un paio di punti prima dello stallo
+cl_f = interp1(pol.alpha, pol.CL, a_vec);
+
+% linear regression
+p      = polyfit(a_vec, cl_f, 1);
+cl0    = p(2);% cl0
+alpha0 = p(2)/p(1);
+%% CL flat plate analogue
+alpha_long = linspace(-180, 180, 361);
+CL_90  = 0.08; %tipical value
+delta1 = 57.6*CL_90*sin(alpha_long*pi/180);
+delta2 = alpha0*cos(alpha_long*pi/180);
+beta   = alpha_long - delta1 - delta2;
+A      = 1 + cl0/sin(pi/4)*sin(alpha_long*pi/180);
+% CL curved plate basic
+CL_cPB = A * CD_90 .* sin(beta*pi/180).*cos(beta*pi/180);
+
+%% changing the value with the one computed by Xfoil
+ii_max = find(alpha_long==min(pol.alpha));
+ii_min = find(alpha_long==max(pol.alpha));
+
+alpha_fin = [ alpha_long(1:ii_max-1), pol.alpha', alpha_long(ii_min + 1:end)];
+CL_fin    = [ CL_cPB(1:ii_max-1), pol.CL', CL_cPB(ii_min+1:end)];
+
+%% CD 
+
+CD_cPB =  CD_90 .* sin(alpha_long*pi/180).^2;
+CD_fin    = [ CD_cPB(1:ii_max-1), pol.CD', CD_cPB(ii_min+1:end)];
+
+%% CM 
+%derivinv armline
+offset = @(a) 0.5111 - 0.001337 * a;
+
+slope  = @(a) 0.001653 + 0.00016 * a;
+
+armLine = offset(alpha0) + slope(alpha0) * (alpha_long-90);
+% define armNeg
+xA = abs(alpha0);
+yA = offset(xA) + slope(xA)*(xA-90)
+xB = -180 - xA;
+yB =  offset(xA) + slope(xA)*(90);
+k  = (yB-yA)/(xB-xA);
+armNeg = yA + k.*(alpha_long - xA)
+
+% pagina 27 --> ci sono un po' di errori in quel paper
+% il braccio qui dovrebbe essere plottato fino ad alpha0,
+% ma in quell'intorno c'è il risultarto di Xfoil quindi non ci serve
+% cd has to be added !!
+CM_out = (-CL_cPB.*cos(alpha_long*pi/180)-CD_cPB.*sin(alpha_long*pi/180)).*(armLine-0.25);
+CM_neg = (-CL_cPB.*cos(alpha_long*pi/180)-CD_cPB.*sin(alpha_long*pi/180)).*(armNeg-0.25);
+% add cm given from xfoil
+CM_fin = [ CM_neg(1:ii_max-1), pol.Cm', CM_out(ii_min+1:end)];
+
+%% write cl, cd e cm to file
+coeff360.alpha = alpha_fin;
+coeff360.CL    = CL_fin;
+coeff360.CD    = CD_fin;
+coeff360.CM    = CM_fin;
---
+++ Matlab_Code/Main_FastCatch_360_coupled.m
@@ -0,0 +1,85 @@
+%% Script per creare la Geometria 3D
+clear all 
+close all
+addpath(genpath('BLACKBOX'));
+%% Input Data
+Chord=0.05;
+p_c=20; % numero di profili di "Transizione" nella parte centrale
+l=0.3; % lunghezza della pala avente un profilo 2D definito, NON corrisponde alla lunghezza del boomerang
+delta=40*pi/180; %Angolo di freccia
+beta=0*pi/180; %Angolo di Diedro
+pitch=0*pi/180; %Pitch angle
+num=20; %Numero di profili totale su ciascuna metà;
+PARA=2.; %Parametro che permette di modificare la curvatura centrale (più si avvicna ad 1 pù dietro forma una V
+% Profile 2D Shape
+%% Profilo 2D e caratteristiche aerodinamiche
+Profile2D=importdata('fastcatch.dat');
+Xp = Profile2D.data(:,1).*Chord-Chord;
+Zp = Profile2D.data(:,2).*Chord-max(Profile2D.data(:,2).*Chord)/2;
+Xp_flip = -(Chord/2.*ones(size(Xp))+Xp)+Chord/2.*ones(size(Xp))-Chord;
+Xp_flip = [Xp_flip(numel(Xp_flip)/2+1:end); Xp_flip(1:numel(Xp_flip)/2)];
+Zp_flip = [Zp(numel(Xp_flip)/2+1:end); Zp(1:numel(Xp_flip)/2)];
+%load fastcatch_360.mat
+coeff360  = f_polar_360('load', 'fastcatch.dat', 50000, 1.6);
+CL_t      = coeff360.CL;
+CD_t      = coeff360.CD;
+CM_t      = coeff360.CM;
+alpha_cl  = coeff360.alpha;
+alpha_cd  = coeff360.alpha;
+alpha_cm  = coeff360.alpha;
+%% Creazione dell Info Box
+BoomInfo.Pianta.l=l;
+BoomInfo.Pianta.freccia=delta;
+BoomInfo.Pianta.diedro=beta;
+BoomInfo.Pianta.pitch=pitch;
+BoomInfo.Geom3D.p_c=p_c;
+BoomInfo.Geom3D.num=num;
+BoomInfo.Geom3D.PARA=PARA;
+BoomInfo.Profile.Chord=Chord;
+BoomInfo.Profile.Xp_sx=Xp;
+BoomInfo.Profile.Xp_dx=Xp_flip;
+BoomInfo.Profile.Zp_sx=Zp;
+BoomInfo.Profile.Zp_dx=Zp_flip;
+BoomInfo.Aero.alpha_cl=alpha_cl;
+BoomInfo.Aero.alpha_cd=alpha_cd;
+BoomInfo.Aero.alpha_cm=alpha_cm;
+BoomInfo.Aero.Cl=CL_t;
+BoomInfo.Aero.Cd=CD_t;
+BoomInfo.Aero.Cm=CM_t;
+BoomInfo.Mecc.Dens=650;
+%%
+[BoomInfo] = Boom3DShape(BoomInfo,'Info','Create_Stl');
+CheckBoomInfo(BoomInfo,'Plot')
+%% Initial Condition
+X_ini=[8.7 6.8 7.3 56.6 10.8]*10;
+X_ini=[10.0000    9.9000    1.6000   65.5000    8.3000]*10;
+r0=X_ini(1)*2*pi/10;
+theta=X_ini(2)*pi/180/10;
+D=X_ini(3)*pi/180/10;
+phi=X_ini(4)*pi/180/10;
+Vs=X_ini(5)/10;
+theta0=0*pi/180;
+phi0=0*pi/180;
+psi0=0*pi/180;
+Tl_0=[cos(theta0)*cos(psi0), cos(theta0)*sin(psi0), -sin(theta0)
+    -cos(phi0)*sin(psi0)+sin(phi0)*sin(theta0)*cos(psi0), cos(phi0)*cos(psi0)+sin(phi0)*sin(theta0)*sin(psi0), sin(phi0)*cos(theta0)
+    sin(phi0)*sin(psi0)+cos(phi0)*sin(theta0)*cos(psi0), -sin(phi0)*cos(psi0)+cos(phi0)*sin(theta0)*sin(psi0), cos(phi0)*cos(theta0)];
+z0= 1.8; % initial altitude
+[quat,ustart] = HandInitial(r0,theta,D,phi,Vs,Tl_0,BoomInfo);
+tfin=40;
+[V_dx_b,V_sx_b]=InitialConditionPlot(Tl_0,quatToAtt(quat),ustart',[0;0;r0],BoomInfo);
+
+%%
+options = odeset('Events', @EventsQUAT,'RelTol',1e-4,'AbsTol',1e-6);
+Y0=[quat 0 0 r0  ustart(1) ustart(2) ustart(3) 0 0 z0 ]';
+
+tic
+[TOUT,YOUT_quat] = ode45(@(t,y)EquationOfMotionsQuaternion_IND(t,y,BoomInfo,Tl_0),[0 tfin],Y0,options); %
+toc
+%% Grafici Finali
+[YOUT] = Eul_Quat(YOUT_quat,TOUT);
+Energy(TOUT,YOUT,BoomInfo)
+PlotTipDxSx(TOUT,YOUT,BoomInfo,Tl_0)
+%PlotAeroForce(YOUT,TOUT,BoomInfo)
+ChiAvan(BoomInfo,YOUT,TOUT)
+FinalReport(YOUT,TOUT)
