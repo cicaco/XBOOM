543cab75d1431ffcd9763e0620d565dfb913b297
0OLRAC0 <carlo2.cordoni@mail.polimi.it>
2022-07-04 17:02:19 +0200

    New Event

--- Matlab_Code/BLACKBOX/Mecc/EventsQUAT.m
+++ Matlab_Code/BLACKBOX/Mecc/EventsQUAT.m
@@ -19,10 +19,46 @@ function [value, isterminal, direction] = EventsQUAT(T, Y)
 % - direction  ??
 value      = (Y(13) <= 0.01); % tocco con il terreno
 
+% if T>2 && value==0
+%     value = (sqrt(Y(11)^2+Y(12)^2+(Y(13)-1.8)^2)<=3); % distanza dal punto di lancio 
+%     %a cui interrompo l'integrazione: 3 metri
+% end
+
 if T>2 && value==0
-    value = (sqrt(Y(11)^2+Y(12)^2+Y(13)^2)<=3); % distanza dal punto di lancio 
+    if sqrt(Y(11)^2+Y(12)^2)<=3 && Y(13)<=4
+    value = 1; % distanza dal punto di lancio 
+    end
     %a cui interrompo l'integrazione: 3 metri
 end
+
+if T>2 && value==0 && Y(9)>0
+    value= (atan2(Y(10),Y(9))>pi/6);
+end
+
+if T>4
+    value=1;
+end
+% 
+% if T>1.5 && value==0
+%     value = (sqrt(Y(11)^2+Y(12)^2+1.8^2)<=3);
+% end
+
+
+
+% if T>2 && value==0
+%     euler = quatToEuler( quat );
+% 
+%     phi = euler(1);
+%     theta = euler(2);
+%     psi= euler(3);
+%     Rot= [cos(theta)*cos(psi) cos(theta)*sin(psi) -sin(theta);
+%         cos(phi)*sin(psi)+sin(phi)*sin(theta)*cos(psi) cos(phi)*cos(psi)+sin(phi)*sin(theta)*sin(psi) sin(phi)*cos(theta);
+%         sin(phi)*sin(psi)+cos(phi)*sin(theta)*cos(psi) -sin(phi)*cos(psi)+cos(phi)*sin(theta)*sin(psi)  cos(phi)*cos(theta)];
+%     Omega= Rot*[Y(4) Y(5) Y(6)]'; % vettore velocità angolare rappresentata nel sistema di riferimento inerziale
+%     Omega_xy= [Omega(1) Omega(2) 0];
+%     
+% end
+
 % 
 % if T<1 && value==0
 %     value=( Y(2)>= 90); % controllo sull'inclinazione del boomerang 
@@ -30,4 +66,5 @@ end
 
 isterminal = 1;   % Stop the integration
 direction  = 0;
+
 end
